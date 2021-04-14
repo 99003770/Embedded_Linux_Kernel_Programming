e88c9ba8e84427643342b05cf3a5dce2c821aece
99003770 <78854076+99003770@users.noreply.github.com>
2021-04-14 09:24:15 +0530

    Add files via upload

---
+++ Hands-on/Race condition scenarios/racethread.c
@@ -0,0 +1,55 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/time.h>
+#include <linux/cdev.h>
+
+static struct task_struct *task1;
+static struct task_struct *task2;
+int val=100;
+const int max=10000;
+static int thread_one(void *pargs) {
+int i;
+for(i=1;i<=max;i++)
+{
+val++;
+printk("Stopped Thread A -- %d \n", val);
+}
+do_exit(0);
+return 0;
+}
+
+static int thread_two(void *pargs) {
+int i;
+for(i=1;i<=max;i++)
+{
+
+printk("Stopped Thread B-- %d\n",val);
+}
+do_exit(0);
+return 0;
+}
+static int __init tdemo_init(void)
+{
+task1=kthread_run(thread_one, NULL, "thread_A");
+//kthread_create + wake_up_process
+task2=kthread_run(thread_two, NULL, "thread_B");
+printk("Pseudo Driver Sample...Welcome\n");
+return 0;
+}
+static void __exit tdemo_exit(void)
+{
+if(task1)
+kthread_stop(task1);
+if(task2)
+kthread_stop(task2);
+}
+module_init(tdemo_init);
+module_exit(tdemo_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your name");
+MODULE_DESCRIPTION("Parameter demo Module");
